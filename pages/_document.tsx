0f377531a262249d4b2aed446b1e93c98086ea0e
halawata13 <rsuzuki13@gmail.com>
2021-09-20 10:48:20 +0900

    アイコン変更

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,10 +6,10 @@ export default class Document extends NextDocument {
       <Html>
         <Head>
           <title>Health Diary</title>
-          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'crossOrigin'} />
-          <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;600&display=swap" rel="stylesheet" />
           <link rel="apple-touch-icon" href="/icons/icon-192.png" />
           <link rel="manifest" href="/manifest.json" />
         </Head>
