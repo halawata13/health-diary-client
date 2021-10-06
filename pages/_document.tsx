cb54c2acfea797e98398b4b94542fd6739681e14
halawata13 <rsuzuki13@gmail.com>
2021-10-06 15:59:04 +0900

    アイコン、テーマカラー修正

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default class Document extends NextDocument {
       <Html>
         <Head>
           <title>Health Diary</title>
-          <link rel="icon" type="image/svg+xml" href="/favicon.png" />
+          <link rel="icon" type="image/png" href="/favicon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'crossOrigin'} />
           <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;600&display=swap" rel="stylesheet" />
